c477ac754a516471ed3cb7029d03bacfcaaff6fa
Alex Cawley <alexcawley97@gmail.com>
2018-07-03 20:26:39 -0500

    Added signin, signup, logout backend workflow

--- app/src/main/java/com/example/e4977/spotme/MainMenu.java
+++ app/src/main/java/com/example/e4977/spotme/MainMenu.java
@@ -8,23 +8,66 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import java.util.HashMap;
+
 public class MainMenu
         extends AppCompatActivity
-    {
+{
     private LinearLayout menuWrapper;
+    private Button btnLogout;
+
+    private SQLiteHandler db;
+    private SessionManager session;
+
 
     @Override
     protected void onCreate (Bundle savedInstanceState)
-        {
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
         menuWrapper = findViewById(R.id.menuWrapper);
+        btnLogout = findViewById(R.id.logoutButton);
+
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+
+        if (!session.isLoggedIn())
+        {
+            logoutUser();
         }
 
-        public void intentToContactPage(View view)
+        // Fetching user details from sqlite
+        HashMap<String, String> user = db.getUserDetails();
+
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener()
         {
-            Intent intent = new Intent(this, ContactActivity.class);
-            startActivity(intent);
-        }
+            @Override
+            public void onClick(View v) {
+                logoutUser();
+            }
+        });
     }
+
+    public void intentToContactPage(View view)
+    {
+        Intent intent = new Intent(this, ContactActivity.class);
+        startActivity(intent);
+    }
+
+    private void logoutUser()
+    {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainMenu.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
